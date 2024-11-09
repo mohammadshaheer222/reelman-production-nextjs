d4a7fef94d0ee61f3983b7c941590958c24dfaef
mohammad shaheer <shaheercse2022@gmail.com>
2024-11-09 14:39:21 +0530

    Added Middle slider section

--- src/app/(build_elements)/_components/User/slider-element.tsx
+++ src/app/(build_elements)/_components/User/slider-element.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
@@ -7,9 +7,34 @@ import "slick-carousel/slick/slick-theme.css";
 import "react-lazy-load-image-component/src/effects/blur.css";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 
-const SliderElement = ({ sliderImage }: { sliderImage: string }) => {
+type SliderImage = {
+  image: string,
+}
+
+const SliderElement = ({ sliderImage, isParallax }: { sliderImage: SliderImage[], isParallax?: boolean | undefined }) => {
   const [imageLoader, setImageLoader] = useState(true);
-  const infinite = Array.isArray(sliderImage) && sliderImage.length > 1;
+  const infinite = sliderImage.length > 1;
+  const sliderRef = useRef<HTMLDivElement | null>(null);
+  // const [isSliderVisible, setIsSliderVisible] = useState(true);
+
+  // useEffect(() => {
+  //   const observer = new IntersectionObserver(
+  //     ([entry]) => {
+  //       if (entry.isIntersecting) {
+  //         setIsSliderVisible(true); 
+  //       }
+  //     },
+  //     { threshold: 0.5 }
+  //   );
+  //   if (sliderRef.current) {
+  //     observer.observe(sliderRef.current);
+  //   }
+  //   return () => {
+  //     if (sliderRef.current) {
+  //       observer.unobserve(sliderRef.current);
+  //     }
+  //   };
+  // }, []);
 
   const settings = {
     dots: infinite,
@@ -19,59 +44,86 @@ const SliderElement = ({ sliderImage }: { sliderImage: string }) => {
     slidesToScroll: 1,
     arrows: false,
     autoplay: true,
-    autoplaySpeed: 4000,
+    autoplaySpeed: 4,
   };
 
   return (
-    <Slider {...settings}>
-      <Parallax
-        strength={200}
-        bgImageStyle={{
-          objectFit: "cover",
-          objectPosition: "center",
-          width: "100%",
-          height: "100vh",
-        }}
-      >
-        {imageLoader && (
-          <div style={{
-            width: "100vw",
-            height: "95vh",
-            backgroundColor: "gray",
-            animation: "pulse 1.5s infinite",
-            position: "absolute",
-            top: 0,
-            left: 0,
-            zIndex: 1,
-          }}/>
-        ) }
-        <LazyLoadImage
-          src={sliderImage ? sliderImage : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmCS1UA3pZIonRuREw2OLe4fXDUlTwPazCCGpk8W3zHM5OmH7FYx0C2ohARdFSzVN5TUg&usqp=CAU"}
-          alt="Slider background"
-          effect="blur"
-          onLoad={() => setImageLoader(false)}
-          style={{
-            width: "100vw",
-            height: "95vh",
-            objectFit: "cover",
-            objectPosition: "center",
-            opacity: imageLoader ? 0 : 1,
-            transition: "opacity 0.5s ease-in-out",
-            position: "relative",
-            zIndex: 2,
-          }}
-        />
-        {/* <div
-          style={{
-            height: "95vh",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        ></div> */}
-      </Parallax>
-    </Slider>
-  );
+
+    <div ref={sliderRef}>
+      <Slider {...settings}>
+        {sliderImage.map((image: SliderImage, index) => (
+          isParallax ? (
+            <>
+              {imageLoader && (
+                <div style={{
+                  width: "100vw",
+                  height: "95vh",
+                  backgroundColor: "gray",
+                  animation: "pulse 1.5s infinite",
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                  zIndex: 1,
+                }} />
+              )}
+              <Parallax
+                key={index}
+                strength={200}
+                bgImageStyle={{
+                  objectFit: "cover",
+                  objectPosition: "center",
+                  width: "100%",
+                  height: "100vh",
+                }}
+                bgImage={image.image ? image.image : ""}
+                onLoad={() => setImageLoader(false)}
+                style={{
+                  height: "95vh",
+                  width: "100vw",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+              </Parallax>
+            </>
+          ) : (
+            <>
+              {imageLoader && (
+                <div style={{
+                  width: "100vw",
+                  height: "95vh",
+                  backgroundColor: "gray",
+                  animation: "pulse 1.5s infinite",
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                  zIndex: 1,
+                }} />
+              )}
+              <LazyLoadImage
+                src={image.image ? image.image : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmCS1UA3pZIonRuREw2OLe4fXDUlTwPazCCGpk8W3zHM5OmH7FYx0C2ohARdFSzVN5TUg&usqp=CAU"}
+                alt="Slider background"
+                effect="blur"
+                onLoad={() => setImageLoader(false)}
+                style={{
+                  width: "100vw",
+                  height: "95vh",
+                  objectFit: "cover",
+                  objectPosition: "center",
+                  opacity: imageLoader ? 0 : 1,
+                  transition: "opacity 0.5s ease-in-out",
+                  position: "relative",
+                  zIndex: 2,
+                }}
+              />
+            </>
+          )
+
+        ))}
+      </Slider>
+    </div>
+  )
 };
 
 export default SliderElement;
--- src/app/_components/page-content.tsx
+++ src/app/_components/page-content.tsx
@@ -5,9 +5,12 @@ import React from "react";
 import { SliderElement } from "../(build_elements)/_components/index";
 
 export default function PageContent() {
+  const heroImage = [{image: "https://lioneltan.co.nz/wp-content/uploads/2020/04/orakei-bay-wedding-46.jpg"},{image: "https://lioneltan.co.nz/wp-content/uploads/2020/04/orakei-bay-wedding-46.jpg"}, ]
     return (
       <>
-        <SliderElement sliderImage="https://lioneltan.co.nz/wp-content/uploads/2020/04/orakei-bay-wedding-46.jpg" />
+        <SliderElement sliderImage={heroImage} isParallax={true} />
+        {/* <SliderElement sliderImage={heroImage}  /> */}
+        <div className="h-screen"></div>
       </>
     )
 }
